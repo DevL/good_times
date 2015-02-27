3c31b8ffd2887eeafd83cf538ed9545a2ab23958
Lennart Fridén <lennart@devl.se>
2015-02-27 17:50:52 +0100

    Add tests for seconds_ago and seconds_from_now

--- test/good_times_test.exs
+++ test/good_times_test.exs
@@ -1,18 +1,23 @@
 defmodule GoodTimesTest do
   use ExUnit.Case
   import GoodTimes
+  import Mock
 
   test "now" do
     assert now == :calendar.universal_time
   end
 
-  @tag :pending
   test "seconds_from_now" do
-    assert true
+    expected = {0, {0, 0, 10}}
+    difference = :calendar.time_difference now, seconds_from_now(10)
+
+    assert difference == expected
   end
 
-  @tag :pending
   test "seconds_ago" do
-    assert true
+    expected = {-1, {23, 59, 45}}
+    difference = :calendar.time_difference now, seconds_ago(15)
+
+    assert difference == expected
   end
 end
