7f8922acad20dd4b0478d8fc11bf585d9f62687b
Lennart Fridén <lennart@devl.se>
2015-02-27 19:41:32 +0100

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GoodTimes.Mixfile do
   def project do
     [
       app: :good_times,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.0",
       deps: deps,
       description: description,
