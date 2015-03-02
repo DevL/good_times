46575bd99a2d16c1f5e3b6c1e19b818726022b2c
Martin Svalin <martin@lite.nu>
2015-03-02 22:00:29 +0100

    Meta adds the rest of our functions

--- lib/meta/meta.ex
+++ lib/meta/meta.ex
@@ -5,12 +5,12 @@ defmodule GoodTimes.Meta do
     time_units = "#{time_unit}s" # TODO: pass in optional plural
 
     time_units_after = String.to_atom "#{time_units}_after"
-    _a_time_unit_after = String.to_atom "#{a}_#{time_unit}_after"
-    _time_units_before = String.to_atom "#{time_units}_before"
-    _a_time_unit_before = String.to_atom "#{a}_#{time_unit}_before"
-    _time_units_from_now = String.to_atom "#{time_units}_from_now"
+    a_time_unit_after = String.to_atom "#{a}_#{time_unit}_after"
+    time_units_before = String.to_atom "#{time_units}_before"
+    a_time_unit_before = String.to_atom "#{a}_#{time_unit}_before"
+    time_units_from_now = String.to_atom "#{time_units}_from_now"
     a_time_unit_from_now = String.to_atom "#{a}_#{time_unit}_from_now"
-    _time_units_ago = String.to_atom "#{time_units}_ago"
+    time_units_ago = String.to_atom "#{time_units}_ago"
     a_time_unit_ago = String.to_atom "#{a}_#{time_unit}_ago"
 
     quote do
@@ -29,6 +29,76 @@ defmodule GoodTimes.Meta do
 
 
       @doc """
+      Returns the UTC date and time #{unquote a} #{unquote time_unit} after the given datetime.
+
+      ## Examples
+
+          iex> #{unquote a_time_unit_after} #{inspect a_datetime}
+          #{inspect GoodTimes.seconds_after(unquote(seconds_per_time_unit), a_datetime)}
+      """
+      @spec unquote(a_time_unit_after)(datetime) :: datetime
+      def unquote(a_time_unit_after)(datetime) do
+        GoodTimes.seconds_after(unquote(seconds_per_time_unit), datetime)
+      end
+
+
+      @doc """
+      Returns the UTC date and time the specified #{unquote time_units} before the given datetime.
+
+      ## Examples
+
+          iex> 15 |> #{unquote time_units_before} #{inspect a_datetime}
+          #{inspect GoodTimes.seconds_before(15 * unquote(seconds_per_time_unit), a_datetime)}
+      """
+      @spec unquote(time_units_before)(integer, datetime) :: datetime
+      def unquote(time_units_before)(number, datetime) do
+        GoodTimes.seconds_before(number * unquote(seconds_per_time_unit), datetime)
+      end
+
+
+      @doc """
+      Returns the UTC date and time #{unquote a} #{unquote time_unit} after the given datetime.
+
+      ## Examples
+
+          iex> #{unquote a_time_unit_before} #{inspect a_datetime}
+          #{inspect GoodTimes.seconds_before(unquote(seconds_per_time_unit), a_datetime)}
+      """
+      @spec unquote(a_time_unit_before)(datetime) :: datetime
+      def unquote(a_time_unit_before)(datetime) do
+        GoodTimes.seconds_before(unquote(seconds_per_time_unit), datetime)
+      end
+
+
+      @doc """
+      Returns the current UTC time the specified #{unquote time_units} from now.
+
+      ## Examples
+
+          iex> 15 |> #{unquote time_units_from_now}
+          #{inspect GoodTimes.seconds_from_now(15 * unquote seconds_per_time_unit)}
+      """
+      @spec unquote(time_units_from_now)(integer) :: datetime
+      def unquote(time_units_from_now)(number) do
+        GoodTimes.seconds_from_now(number * unquote seconds_per_time_unit)
+      end
+
+
+      @doc """
+      Returns the current UTC time the specified #{unquote time_units} ago.
+
+      ## Examples
+
+          iex> 15 |> #{unquote time_units_ago}
+          #{inspect GoodTimes.seconds_ago(15 * unquote seconds_per_time_unit)}
+      """
+      @spec unquote(time_units_ago)(integer) :: datetime
+      def unquote(time_units_ago)(number) do
+        GoodTimes.seconds_ago(number * unquote seconds_per_time_unit)
+      end
+
+
+      @doc """
       Returns the current UTC time #{unquote a} #{unquote time_unit} from now.
 
       ## Examples
@@ -40,6 +110,7 @@ defmodule GoodTimes.Meta do
       def unquote(a_time_unit_from_now)() do
         GoodTimes.seconds_from_now(unquote seconds_per_time_unit)
       end
+
 
       @doc """
       Returns the current UTC time #{unquote a} #{unquote time_unit} ago.
--- test/meta/meta_test.exs
+++ test/meta/meta_test.exs
@@ -19,35 +19,70 @@ defmodule GoodTimes.MetaTest do
   end
 
   @a_datetime {{2015, 2, 27}, {18, 30, 45}}
-  test "defines hundreds_after" do
+  test "defines hundreds_after/2" do
     expected = {0, {0, 16, 40}}
     actual = difference @a_datetime, A.hundreds_after(10, @a_datetime)
 
     assert actual == expected
   end
 
-  test "defines a_hundred_from_now" do
+  test "defines a_hundred_after/1" do
+    expected = {0, {0, 1, 40}}
+    actual = difference @a_datetime, A.a_hundred_after(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "defines hundreds_before/2" do
+    expected = {-1, {23, 43, 20}}
+    actual = difference @a_datetime, A.hundreds_before(10, @a_datetime)
+
+    assert actual == expected
+  end
+
+  test "defines a_hundred_before/1" do
+    expected = {-1, {23, 58, 20}}
+    actual = difference @a_datetime, A.a_hundred_before(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "defines hundreds_from_now/1" do
+    expected = {0, {0, 16, 40}}
+    actual = difference now, A.hundreds_from_now(10)
+
+    assert actual == expected
+  end
+
+  test "defines hundreds_ago/1" do
+    expected = {-1, {23, 43, 20}}
+    actual = difference now, A.hundreds_ago(10)
+
+    assert actual == expected
+  end
+
+  test "defines a_hundred_from_now/0" do
     expected = {0, {0, 1, 40}}
     actual = difference now, A.a_hundred_from_now
 
     assert actual == expected
   end
 
-  test "defines a_hundred_ago" do
+  test "defines a_hundred_ago/0" do
     expected = {-1, {23, 58, 20}}
     actual = difference now, A.a_hundred_ago
 
     assert actual == expected
   end
 
-  test "defines an_eight_from_now" do
+  test "defines an_eight_from_now/0" do
     expected = {0, {0, 0, 8}}
     actual = difference now, A.an_eight_from_now
 
     assert actual == expected
   end
 
-  test "defines an_eight_ago" do
+  test "defines an_eight_ago/0" do
     expected = {-1, {23, 59, 52}}
     actual = difference now, A.an_eight_ago
 
