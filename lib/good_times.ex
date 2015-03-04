78c2a8fdf49741ab2c177da552b00e450253a37f
Martin Svalin <martin@lite.nu>
2015-03-04 20:18:41 +0100

    Reorder functions and specs
    Functions and specs should be in the same order to
    make them easier to navigate. I put them in what,
    imho, is a more natural order:
    
    - ´<time_units>_after´
    - ´<time_units>_before´
    - ´a_<time_unit>_after´
    - ´a_<time_unit>_before´
    - ´<time_units>_from_now´
    - ´<time_units>_ago´
    - ´a_<time_unit>_from_now´
    - ´a_<time_unit>_ago´

--- lib/good_times.ex
+++ lib/good_times.ex
@@ -7,8 +7,8 @@ defmodule GoodTimes do
   """
 
   @seconds_per_minute 60
-  @seconds_per_hour 3600
-  @seconds_per_day 86400
+  @seconds_per_hour 60*60
+  @seconds_per_day 24*60*60
 
   @type year  :: non_neg_integer
   @type month :: 1..12
@@ -46,17 +46,6 @@ defmodule GoodTimes do
   end
 
   @doc """
-  Returns the UTC date and time a second after the given datetime.
-
-  ## Examples
-
-      iex> a_second_after {{2015, 2, 27}, {18, 30, 45}}
-      {{2015, 2, 27}, {18, 30, 46}}
-  """
-  @spec a_second_after(datetime) :: datetime
-  def a_second_after(datetime), do: seconds_after(1, datetime)
-
-  @doc """
   Returns the UTC date and time the specified seconds before the given datetime.
 
   ## Examples
@@ -66,6 +55,17 @@ defmodule GoodTimes do
   """
   @spec seconds_before(integer, datetime) :: datetime
   def seconds_before(seconds, datetime), do: seconds_after(-seconds, datetime)
+
+  @doc """
+  Returns the UTC date and time a second after the given datetime.
+
+  ## Examples
+
+      iex> a_second_after {{2015, 2, 27}, {18, 30, 45}}
+      {{2015, 2, 27}, {18, 30, 46}}
+  """
+  @spec a_second_after(datetime) :: datetime
+  def a_second_after(datetime), do: seconds_after(1, datetime)
 
   @doc """
   Returns the UTC date and time a second before the given datetime.
@@ -90,17 +90,6 @@ defmodule GoodTimes do
   def seconds_from_now(seconds), do: seconds_after(seconds, now)
 
   @doc """
-  Returns the UTC date and time a second from now.
-
-   ## Examples
-
-      iex> a_second_from_now
-      {{2015, 2, 27}, {18, 30, 46}}
-  """
-  @spec a_second_from_now :: datetime
-  def a_second_from_now, do: seconds_from_now(1)
-
-  @doc """
   Returns the UTC date and time the specified seconds ago.
 
   ## Examples
@@ -110,6 +99,17 @@ defmodule GoodTimes do
   """
   @spec seconds_ago(integer) :: datetime
   def seconds_ago(seconds), do: seconds_before(seconds, now)
+
+  @doc """
+  Returns the UTC date and time a second from now.
+
+   ## Examples
+
+      iex> a_second_from_now
+      {{2015, 2, 27}, {18, 30, 46}}
+  """
+  @spec a_second_from_now :: datetime
+  def a_second_from_now, do: seconds_from_now(1)
 
   @doc """
   Returns the UTC date and time a second ago.
@@ -131,17 +131,6 @@ defmodule GoodTimes do
   def minutes_after(minutes, datetime), do: seconds_after(minutes * @seconds_per_minute, datetime)
 
   @doc """
-  Returns the UTC date and time a minute after the given datetime.
-
-  ## Examples
-
-      iex> a_minute_after {{2015, 2, 27}, {18, 30, 45}}
-      {{2015, 2, 27}, {18, 31, 45}}
-  """
-  @spec a_minute_after(datetime) :: datetime
-  def a_minute_after(datetime), do: minutes_after(1, datetime)
-
-  @doc """
   Returns the UTC date and time the specified minutes before the given datetime.
 
   ## Examples
@@ -151,6 +140,17 @@ defmodule GoodTimes do
   """
   @spec minutes_before(integer, datetime) :: datetime
   def minutes_before(minutes, datetime), do: seconds_before(minutes * @seconds_per_minute, datetime)
+
+  @doc """
+  Returns the UTC date and time a minute after the given datetime.
+
+  ## Examples
+
+      iex> a_minute_after {{2015, 2, 27}, {18, 30, 45}}
+      {{2015, 2, 27}, {18, 31, 45}}
+  """
+  @spec a_minute_after(datetime) :: datetime
+  def a_minute_after(datetime), do: minutes_after(1, datetime)
 
   @doc """
   Returns the UTC date and time a minute before the given datetime.
@@ -175,17 +175,6 @@ defmodule GoodTimes do
   def minutes_from_now(minutes), do: minutes_after(minutes, now)
 
   @doc """
-  Returns the UTC date and time a minute from now.
-
-   ## Examples
-
-      iex> a_minute_from_now
-      {{2015, 2, 27}, {18, 31, 45}}
-  """
-  @spec a_minute_from_now :: datetime
-  def a_minute_from_now, do: minutes_from_now(1)
-
-  @doc """
   Returns the UTC date and time the specified minutes ago.
 
   ## Examples
@@ -195,6 +184,17 @@ defmodule GoodTimes do
   """
   @spec minutes_ago(integer) :: datetime
   def minutes_ago(minutes), do: minutes_before(minutes, now)
+
+  @doc """
+  Returns the UTC date and time a minute from now.
+
+   ## Examples
+
+      iex> a_minute_from_now
+      {{2015, 2, 27}, {18, 31, 45}}
+  """
+  @spec a_minute_from_now :: datetime
+  def a_minute_from_now, do: minutes_from_now(1)
 
   @doc """
   Returns the UTC date and time a minute ago.
@@ -219,17 +219,6 @@ defmodule GoodTimes do
   def hours_after(hours, datetime), do: seconds_after(hours * @seconds_per_hour, datetime)
 
   @doc """
-  Returns the UTC date and time an hour after the given datetime.
-
-  ## Examples
-
-      iex> an_hour_after {{2015, 2, 27}, {18, 30, 45}}
-      {{2015, 2, 27}, {19, 30, 45}}
-  """
-  @spec an_hour_after(datetime) :: datetime
-  def an_hour_after(datetime), do: hours_after(1, datetime)
-
-  @doc """
   Returns the UTC date and time the specified hours before the given datetime.
 
   ## Examples
@@ -239,6 +228,17 @@ defmodule GoodTimes do
   """
   @spec hours_before(integer, datetime) :: datetime
   def hours_before(hours, datetime), do: seconds_before(hours * @seconds_per_hour, datetime)
+
+  @doc """
+  Returns the UTC date and time an hour after the given datetime.
+
+  ## Examples
+
+      iex> an_hour_after {{2015, 2, 27}, {18, 30, 45}}
+      {{2015, 2, 27}, {19, 30, 45}}
+  """
+  @spec an_hour_after(datetime) :: datetime
+  def an_hour_after(datetime), do: hours_after(1, datetime)
 
   @doc """
   Returns the UTC date and time an hour before the given datetime.
@@ -263,17 +263,6 @@ defmodule GoodTimes do
   def hours_from_now(hours), do: hours_after(hours, now)
 
   @doc """
-  Returns the UTC date and time an hour from now.
-
-   ## Examples
-
-      iex> an_hour_from_now
-      {{2015, 2, 27}, {19, 30, 45}}
-  """
-  @spec an_hour_from_now :: datetime
-  def an_hour_from_now, do: hours_from_now(1)
-
-  @doc """
   Returns the UTC date and time the specified hours ago.
 
   ## Examples
@@ -283,6 +272,17 @@ defmodule GoodTimes do
   """
   @spec hours_ago(integer) :: datetime
   def hours_ago(hours), do: hours_before(hours, now)
+
+  @doc """
+  Returns the UTC date and time an hour from now.
+
+   ## Examples
+
+      iex> an_hour_from_now
+      {{2015, 2, 27}, {19, 30, 45}}
+  """
+  @spec an_hour_from_now :: datetime
+  def an_hour_from_now, do: hours_from_now(1)
 
   @doc """
   Returns the UTC date and time an hour ago.
@@ -307,17 +307,6 @@ defmodule GoodTimes do
   def days_after(days, datetime), do: seconds_after(days * @seconds_per_day, datetime)
 
   @doc """
-  Returns the UTC date and time a day after the given datetime.
-
-  ## Examples
-
-      iex> a_day_after {{2015, 2, 27}, {18, 30, 45}}
-      {{2015, 2, 28}, {18, 30, 45}}
-  """
-  @spec a_day_after(datetime) :: datetime
-  def a_day_after(datetime), do: days_after(1, datetime)
-
-  @doc """
   Returns the UTC date and time the specified days before the given datetime.
 
   ## Examples
@@ -327,6 +316,17 @@ defmodule GoodTimes do
   """
   @spec days_before(integer, datetime) :: datetime
   def days_before(days, datetime), do: seconds_before(days * @seconds_per_day, datetime)
+
+  @doc """
+  Returns the UTC date and time a day after the given datetime.
+
+  ## Examples
+
+      iex> a_day_after {{2015, 2, 27}, {18, 30, 45}}
+      {{2015, 2, 28}, {18, 30, 45}}
+  """
+  @spec a_day_after(datetime) :: datetime
+  def a_day_after(datetime), do: days_after(1, datetime)
 
   @doc """
   Returns the UTC date and time a day before the given datetime.
@@ -351,17 +351,6 @@ defmodule GoodTimes do
   def days_from_now(days), do: days_after(days, now)
 
   @doc """
-  Returns the UTC date and time a day from now.
-
-   ## Examples
-
-      iex> a_day_from_now
-      {{2015, 2, 28}, {18, 30, 45}}
-  """
-  @spec a_day_from_now :: datetime
-  def a_day_from_now, do: days_from_now(1)
-
-  @doc """
   Returns the UTC date and time the specified days ago.
 
   ## Examples
@@ -371,6 +360,17 @@ defmodule GoodTimes do
   """
   @spec days_ago(integer) :: datetime
   def days_ago(days), do: days_before(days, now)
+
+  @doc """
+  Returns the UTC date and time a day from now.
+
+   ## Examples
+
+      iex> a_day_from_now
+      {{2015, 2, 28}, {18, 30, 45}}
+  """
+  @spec a_day_from_now :: datetime
+  def a_day_from_now, do: days_from_now(1)
 
   @doc """
   Returns the UTC date and time a day ago.
--- test/good_times_test.exs
+++ test/good_times_test.exs
@@ -21,48 +21,6 @@ defmodule GoodTimesTest do
     assert actual == expected
   end
 
-  test "minutes_after" do
-    expected = {0, {0, 10, 0}}
-    actual = difference @a_datetime, minutes_after(10, @a_datetime)
-
-    assert actual == expected
-  end
-
-  test "minutes_before" do
-    expected = {-1, {23, 50, 0}}
-    actual = difference @a_datetime, minutes_before(10, @a_datetime)
-
-    assert actual == expected
-  end
-
-  test "hours_after" do
-    expected = {0, {10, 0, 0}}
-    actual = difference @a_datetime, hours_after(10, @a_datetime)
-
-    assert actual == expected
-  end
-
-  test "hours_before" do
-    expected = {-1, {14, 0, 0}}
-    actual = difference @a_datetime, hours_before(10, @a_datetime)
-
-    assert actual == expected
-  end
-
-  test "days_after" do
-    expected = {3, {0, 0, 0}}
-    actual = difference @a_datetime, days_after(3, @a_datetime)
-
-    assert actual == expected
-  end
-
-  test "days_before" do
-    expected = {-3, {0, 0, 0}}
-    actual = difference @a_datetime, days_before(3, @a_datetime)
-
-    assert actual == expected
-  end
-
   test "a_second_after" do
     expected = {0, {0, 0, 1}}
     actual = difference @a_datetime, a_second_after(@a_datetime)
@@ -73,48 +31,6 @@ defmodule GoodTimesTest do
   test "a_second_before" do
     expected = {-1, {23, 59, 59}}
     actual = difference @a_datetime, a_second_before(@a_datetime)
-
-    assert actual == expected
-  end
-
-  test "a_minute_after" do
-    expected = {0, {0, 1, 0}}
-    actual = difference @a_datetime, a_minute_after(@a_datetime)
-
-    assert actual == expected
-  end
-
-  test "a_minute_before" do
-    expected = {-1, {23, 59, 0}}
-    actual = difference @a_datetime, a_minute_before(@a_datetime)
-
-    assert actual == expected
-  end
-
-  test "an_hour_after" do
-    expected = {0, {1, 0, 0}}
-    actual = difference @a_datetime, an_hour_after(@a_datetime)
-
-    assert actual == expected
-  end
-
-  test "an_hour_before" do
-    expected = {-1, {23, 0, 0}}
-    actual = difference @a_datetime, an_hour_before(@a_datetime)
-
-    assert actual == expected
-  end
-
-  test "a_day_after" do
-    expected = {1, {0, 0, 0}}
-    actual = difference @a_datetime, a_day_after(@a_datetime)
-
-    assert actual == expected
-  end
-
-  test "a_day_before" do
-    expected = {-1, {0, 0, 0}}
-    actual = difference @a_datetime, a_day_before(@a_datetime)
 
     assert actual == expected
   end
@@ -133,48 +49,6 @@ defmodule GoodTimesTest do
     assert actual == expected
   end
 
-  test "minutes_from_now" do
-    expected = {0, {0, 5, 0}}
-    actual = difference now, minutes_from_now(5)
-
-    assert actual == expected
-  end
-
-  test "minutes_ago" do
-    expected = {-1, {23, 40, 0}}
-    actual = difference now, minutes_ago(20)
-
-    assert actual == expected
-  end
-
-  test "hours_from_now" do
-    expected = {0, {2, 0, 0}}
-    actual = difference now, hours_from_now(2)
-
-    assert actual == expected
-  end
-
-  test "hours_ago" do
-    expected = {-1, {20, 0, 0}}
-    actual = difference now, hours_ago(4)
-
-    assert actual == expected
-  end
-
-  test "days_from_now" do
-    expected = {3, {0, 0, 0}}
-    actual = difference now, days_from_now(3)
-
-    assert actual == expected
-  end
-
-  test "days_ago" do
-    expected = {-7, {0, 0, 0}}
-    actual = difference now, days_ago(7)
-
-    assert actual == expected
-  end
-
   test "a_second_from_now" do
     expected = {0, {0, 0, 1}}
     actual = difference now, a_second_from_now
@@ -185,6 +59,48 @@ defmodule GoodTimesTest do
   test "a_second_ago" do
     expected = {-1, {23, 59, 59}}
     actual = difference now, a_second_ago
+
+    assert actual == expected
+  end
+
+  test "minutes_after" do
+    expected = {0, {0, 10, 0}}
+    actual = difference @a_datetime, minutes_after(10, @a_datetime)
+
+    assert actual == expected
+  end
+
+  test "minutes_before" do
+    expected = {-1, {23, 50, 0}}
+    actual = difference @a_datetime, minutes_before(10, @a_datetime)
+
+    assert actual == expected
+  end
+
+  test "a_minute_after" do
+    expected = {0, {0, 1, 0}}
+    actual = difference @a_datetime, a_minute_after(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "a_minute_before" do
+    expected = {-1, {23, 59, 0}}
+    actual = difference @a_datetime, a_minute_before(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "minutes_from_now" do
+    expected = {0, {0, 5, 0}}
+    actual = difference now, minutes_from_now(5)
+
+    assert actual == expected
+  end
+
+  test "minutes_ago" do
+    expected = {-1, {23, 40, 0}}
+    actual = difference now, minutes_ago(20)
 
     assert actual == expected
   end
@@ -203,6 +119,48 @@ defmodule GoodTimesTest do
     assert actual == expected
   end
 
+  test "hours_after" do
+    expected = {0, {10, 0, 0}}
+    actual = difference @a_datetime, hours_after(10, @a_datetime)
+
+    assert actual == expected
+  end
+
+  test "hours_before" do
+    expected = {-1, {14, 0, 0}}
+    actual = difference @a_datetime, hours_before(10, @a_datetime)
+
+    assert actual == expected
+  end
+
+  test "an_hour_after" do
+    expected = {0, {1, 0, 0}}
+    actual = difference @a_datetime, an_hour_after(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "an_hour_before" do
+    expected = {-1, {23, 0, 0}}
+    actual = difference @a_datetime, an_hour_before(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "hours_from_now" do
+    expected = {0, {2, 0, 0}}
+    actual = difference now, hours_from_now(2)
+
+    assert actual == expected
+  end
+
+  test "hours_ago" do
+    expected = {-1, {20, 0, 0}}
+    actual = difference now, hours_ago(4)
+
+    assert actual == expected
+  end
+
   test "an_hour_from_now" do
     expected = {0, {1, 0, 0}}
     actual = difference now, an_hour_from_now
@@ -213,6 +171,48 @@ defmodule GoodTimesTest do
   test "an_hour_ago" do
     expected = {-1, {23, 0, 0}}
     actual = difference now, an_hour_ago
+
+    assert actual == expected
+  end
+
+  test "days_after" do
+    expected = {3, {0, 0, 0}}
+    actual = difference @a_datetime, days_after(3, @a_datetime)
+
+    assert actual == expected
+  end
+
+  test "days_before" do
+    expected = {-3, {0, 0, 0}}
+    actual = difference @a_datetime, days_before(3, @a_datetime)
+
+    assert actual == expected
+  end
+
+  test "a_day_after" do
+    expected = {1, {0, 0, 0}}
+    actual = difference @a_datetime, a_day_after(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "a_day_before" do
+    expected = {-1, {0, 0, 0}}
+    actual = difference @a_datetime, a_day_before(@a_datetime)
+
+    assert actual == expected
+  end
+
+  test "days_from_now" do
+    expected = {3, {0, 0, 0}}
+    actual = difference now, days_from_now(3)
+
+    assert actual == expected
+  end
+
+  test "days_ago" do
+    expected = {-7, {0, 0, 0}}
+    actual = difference now, days_ago(7)
 
     assert actual == expected
   end
