0ec3c5f4db418e281d3c971c2ba9547e400ed165
Martin Svalin <martin@lite.nu>
2015-03-08 20:26:48 +0100

    Refactor tests
    More straight-forward tests, with plain assertion in each test case. Functions
    relative to `now` are tested by using their counterpart accepting a datetime,
    and passing in `now` to get the expected value.

--- test/good_times_test.exs
+++ test/good_times_test.exs
@@ -8,163 +8,163 @@ defmodule GoodTimesTest do
 
   @a_datetime {{2015, 2, 27}, {18, 30, 45}}
   test "seconds_after" do
-    assert_difference @a_datetime, seconds_after(10, @a_datetime), {0, {0, 0, 10}}
+    assert seconds_after(10, @a_datetime) == {{2015, 2, 27}, {18, 30, 55}}
   end
 
   test "seconds_before" do
-    assert_difference @a_datetime, seconds_before(10, @a_datetime), {-1, {23, 59, 50}}
+    assert seconds_before(10, @a_datetime) == {{2015, 2, 27}, {18, 30, 35}}
   end
 
   test "a_second_after" do
-    assert_difference @a_datetime, a_second_after(@a_datetime), {0, {0, 0, 1}}
+    assert a_second_after(@a_datetime) == {{2015, 2, 27}, {18, 30, 46}}
   end
 
   test "a_second_before" do
-    assert_difference @a_datetime, a_second_before(@a_datetime), {-1, {23, 59, 59}}
+    assert a_second_before(@a_datetime) == {{2015, 2, 27}, {18, 30, 44}}
   end
 
   test "seconds_from_now" do
-    assert_difference seconds_from_now(10), {0, {0, 0, 10}}
+    assert seconds_from_now(10) == seconds_after(10, now)
   end
 
   test "seconds_ago" do
-    assert_difference seconds_ago(15), {-1, {23, 59, 45}}
+    assert seconds_ago(15) == seconds_before(15, now)
   end
 
   test "a_second_from_now" do
-    assert_difference a_second_from_now, {0, {0, 0, 1}}
+    assert a_second_from_now == a_second_after now
   end
 
   test "a_second_ago" do
-    assert_difference a_second_ago, {-1, {23, 59, 59}}
+    assert a_second_ago == a_second_before now
   end
 
   test "minutes_after" do
-    assert_difference @a_datetime, minutes_after(10, @a_datetime), {0, {0, 10, 0}}
+    assert minutes_after(10, @a_datetime) == {{2015, 2, 27}, {18, 40, 45}}
   end
 
   test "minutes_before" do
-    assert_difference @a_datetime, minutes_before(10, @a_datetime), {-1, {23, 50, 0}}
+    assert minutes_before(10, @a_datetime) == {{2015, 2, 27}, {18, 20, 45}}
   end
 
   test "a_minute_after" do
-    assert_difference @a_datetime, a_minute_after(@a_datetime), {0, {0, 1, 0}}
+    assert a_minute_after(@a_datetime) == {{2015, 2, 27}, {18, 31, 45}}
   end
 
   test "a_minute_before" do
-    assert_difference @a_datetime, a_minute_before(@a_datetime), {-1, {23, 59, 0}}
+    assert a_minute_before(@a_datetime) == {{2015, 2, 27}, {18, 29, 45}}
   end
 
   test "minutes_from_now" do
-    assert_difference minutes_from_now(5), {0, {0, 5, 0}}
+    assert minutes_from_now(5) == minutes_after(5, now)
   end
 
   test "minutes_ago" do
-    assert_difference minutes_ago(20), {-1, {23, 40, 0}}
+    assert minutes_ago(20) == minutes_before(20, now)
   end
 
   test "a_minute_from_now" do
-    assert_difference a_minute_from_now, {0, {0, 1, 0}}
+    assert a_minute_from_now == a_minute_after now
   end
 
   test "a_minute_ago" do
-    assert_difference a_minute_ago, {-1, {23, 59, 0}}
+    assert a_minute_ago == a_minute_before now
   end
 
   test "hours_after" do
-    assert_difference @a_datetime, hours_after(10, @a_datetime), {0, {10, 0, 0}}
+    assert hours_after(10, @a_datetime) == {{2015, 2, 28}, {4, 30, 45}}
   end
 
   test "hours_before" do
-    assert_difference @a_datetime, hours_before(10, @a_datetime), {-1, {14, 0, 0}}
+    assert hours_before(10, @a_datetime) == {{2015, 2, 27}, {8, 30, 45}}
   end
 
   test "an_hour_after" do
-    assert_difference @a_datetime, an_hour_after(@a_datetime), {0, {1, 0, 0}}
+    assert an_hour_after(@a_datetime) == {{2015, 2, 27}, {19, 30, 45}}
   end
 
   test "an_hour_before" do
-    assert_difference @a_datetime, an_hour_before(@a_datetime), {-1, {23, 0, 0}}
+    assert an_hour_before(@a_datetime) == {{2015, 2, 27}, {17, 30, 45}}
   end
 
   test "hours_from_now" do
-    assert_difference hours_from_now(2), {0, {2, 0, 0}}
+    assert hours_from_now(2) == hours_after(2, now)
   end
 
   test "hours_ago" do
-    assert_difference hours_ago(4), {-1, {20, 0, 0}}
+    assert hours_ago(4) == hours_before(4, now)
   end
 
   test "an_hour_from_now" do
-    assert_difference an_hour_from_now, {0, {1, 0, 0}}
+    assert an_hour_from_now == an_hour_after now
   end
 
   test "an_hour_ago" do
-    assert_difference an_hour_ago, {-1, {23, 0, 0}}
+    assert an_hour_ago == an_hour_before now
   end
 
   test "days_after" do
-    assert_difference @a_datetime, days_after(3, @a_datetime), {3, {0, 0, 0}}
+    assert days_after(3, @a_datetime) == {{2015, 3, 2}, {18, 30, 45}}
   end
 
   test "days_before" do
-    assert_difference @a_datetime, days_before(3, @a_datetime), {-3, {0, 0, 0}}
+    assert days_before(3, @a_datetime) == {{2015, 2, 24}, {18, 30, 45}}
   end
 
   test "a_day_after" do
-    assert_difference @a_datetime, a_day_after(@a_datetime), {1, {0, 0, 0}}
+    assert a_day_after(@a_datetime) == {{2015, 2, 28}, {18, 30, 45}}
   end
 
   test "a_day_before" do
-    assert_difference @a_datetime, a_day_before(@a_datetime), {-1, {0, 0, 0}}
+    assert a_day_before(@a_datetime) == {{2015, 2, 26}, {18, 30, 45}}
   end
 
   test "days_from_now" do
-    assert_difference days_from_now(3), {3, {0, 0, 0}}
+    assert days_from_now(3) == days_after(3, now)
   end
 
   test "days_ago" do
-    assert_difference days_ago(7), {-7, {0, 0, 0}}
+    assert days_ago(7) == days_before(7, now)
   end
 
   test "a_day_from_now" do
-    assert_difference a_day_from_now, {1, {0, 0, 0}}
+    assert a_day_from_now == a_day_after now
   end
 
   test "a_day_ago" do
-    assert_difference a_day_ago, {-1, {0, 0, 0}}
+    assert a_day_ago == a_day_before now
   end
 
   test "weeks_after" do
-    assert_difference @a_datetime, weeks_after(4, @a_datetime), {28, {0, 0, 0}}
+    assert weeks_after(4, @a_datetime) == {{2015, 3, 27}, {18, 30, 45}}
   end
 
   test "weeks_before" do
-    assert_difference @a_datetime, weeks_before(4, @a_datetime), {-28, {0, 0, 0}}
+    assert weeks_before(4, @a_datetime) == {{2015, 1, 30}, {18, 30, 45}}
   end
 
   test "a_week_after" do
-    assert_difference @a_datetime, a_week_after(@a_datetime), {7, {0, 0, 0}}
+    assert a_week_after(@a_datetime) == {{2015, 3, 6}, {18, 30, 45}}
   end
 
   test "a_week_before" do
-    assert_difference @a_datetime, a_week_before(@a_datetime), {-7, {0, 0, 0}}
+    assert a_week_before(@a_datetime) == {{2015, 2, 20}, {18, 30, 45}}
   end
 
   test "weeks_from_now" do
-    assert_difference weeks_from_now(4), {28, {0, 0, 0}}
+    assert weeks_from_now(4) == weeks_after(4, now)
   end
 
   test "weeks_ago" do
-    assert_difference weeks_ago(4), {-28, {0, 0, 0}}
+    assert weeks_ago(4) == weeks_before(4, now)
   end
 
   test "a_week_from_now" do
-    assert_difference a_week_from_now, {7, {0, 0, 0}}
+    assert a_week_from_now == a_week_after now
   end
 
   test "a_week_ago" do
-    assert_difference a_week_ago, {-7, {0, 0, 0}}
+    assert a_week_ago == a_week_before now
   end
 
   @last_jan_2015 {{2015, 1, 31}, {12, 0, 0}}
@@ -183,11 +183,11 @@ defmodule GoodTimesTest do
   end
 
   test "a_month_after" do
-    assert_difference @a_datetime, a_month_after(@a_datetime), {28, {0, 0, 0}}
+    assert a_month_after(@a_datetime) == {{2015, 3, 27}, {18, 30, 45}}
   end
 
   test "a_month_before" do
-    assert_difference @a_datetime, a_month_before(@a_datetime), {-31, {0, 0, 0}}
+    assert a_month_before(@a_datetime) == {{2015, 1, 27}, {18, 30, 45}}
   end
 
   test "months_from_now" do
@@ -207,20 +207,20 @@ defmodule GoodTimesTest do
   end
 
   test "years_after" do
-    assert_difference @a_datetime, years_after(4, @a_datetime), {365+366+365+365, {0, 0, 0}}
+    assert years_after(4, @a_datetime) == {{2019, 2, 27}, {18, 30, 45}}
     assert years_after(1, {{2016, 2, 29}, {12, 0, 0}}) == {{2017, 2, 28}, {12, 0, 0}}
   end
 
   test "years_before" do
-    assert_difference @a_datetime, years_before(4, @a_datetime), {-(365+365+366+365), {0, 0, 0}}
+    assert years_before(4, @a_datetime) == {{2011, 2, 27}, {18, 30, 45}}
   end
 
   test "a_year_after" do
-    assert_difference @a_datetime, a_year_after(@a_datetime), {365, {0, 0, 0}}
+    assert a_year_after(@a_datetime) == {{2016, 2, 27}, {18, 30, 45}}
   end
 
   test "a_year_before" do
-    assert_difference @a_datetime, a_year_before(@a_datetime), {-365, {0, 0, 0}}
+    assert a_year_before(@a_datetime) == {{2014, 2, 27}, {18, 30, 45}}
   end
 
   test "years_from_now" do
@@ -237,18 +237,5 @@ defmodule GoodTimesTest do
 
   test "a_year_ago" do
     assert a_year_ago == a_year_before now
-  end
-
-  defp difference(t1, t2) do
-    :calendar.time_difference t1, t2
-  end
-
-  defp assert_difference(datetime, expected) do
-    assert_difference now, datetime, expected
-  end
-
-  defp assert_difference(datetime1 , datetime2, expected) do
-    actual = difference datetime1, datetime2
-    assert actual == expected
   end
 end
