3e45d71cfd442e6f0f999fc3932647f621684384
Martin Svalin <martin@lite.nu>
2015-03-10 20:29:22 +0100

    Add stream functions for currently supported units
    They live in a new module, tentatively called `GoodTimes.Stream`. Each take a
    datetime, and produce a Stream of datetimes. They all start with the given
    datetime, and step one time unit forward or backward per element.
    
    Added functions are:
    
    - stream_seconds_after/1
    - stream_seconds_before/1
    - stream_minutes_after/1
    - stream_minutes_before/1
    - stream_hours_after/1
    - stream_hours_before/1
    - stream_days_after/1
    - stream_days_before/1
    - stream_weeks_after/1
    - stream_weeks_before/1
    - stream_months_after/1
    - stream_months_before/1
    - stream_years_after/1
    - stream_years_before/1

---
+++ lib/good_times/stream.ex
@@ -0,0 +1,234 @@
+defmodule GoodTimes.Stream do
+  import GoodTimes, only: [
+    a_second_after: 1, a_second_before: 1,
+    a_minute_after: 1, a_minute_before: 1,
+    an_hour_after: 1, an_hour_before: 1,
+    a_day_after: 1, a_day_before: 1,
+    a_week_after: 1, a_week_before: 1,
+    a_month_after: 1, a_month_before: 1,
+    a_year_after: 1, a_year_before: 1,
+  ]
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going forward one second at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_seconds_after |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_seconds_after(GoodTimes.datetime) :: Enumerable.t
+  def stream_seconds_after(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_second_after datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going backward one second at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_seconds_before |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_seconds_before(GoodTimes.datetime) :: Enumerable.t
+  def stream_seconds_before(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_second_before datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going forward one minute at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_minutes_after |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_minutes_after(GoodTimes.datetime) :: Enumerable.t
+  def stream_minutes_after(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_minute_after datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going backward one minute at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_minutes_before |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_minutes_before(GoodTimes.datetime) :: Enumerable.t
+  def stream_minutes_before(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_minute_before datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going forward one hour at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_hours_after |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_hours_after(GoodTimes.datetime) :: Enumerable.t
+  def stream_hours_after(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, an_hour_after datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going backward one hour at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_hours_before |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_hours_before(GoodTimes.datetime) :: Enumerable.t
+  def stream_hours_before(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, an_hour_before datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going forward one day at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_days_after |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_days_after(GoodTimes.datetime) :: Enumerable.t
+  def stream_days_after(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_day_after datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going backward one day at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_days_before |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_days_before(GoodTimes.datetime) :: Enumerable.t
+  def stream_days_before(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_day_before datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going forward one week at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_weeks_after |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_weeks_after(GoodTimes.datetime) :: Enumerable.t
+  def stream_weeks_after(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_week_after datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going backward one week at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_weeks_before |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_weeks_before(GoodTimes.datetime) :: Enumerable.t
+  def stream_weeks_before(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_week_before datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going forward one month at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_months_after |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_months_after(GoodTimes.datetime) :: Enumerable.t
+  def stream_months_after(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_month_after datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going backward one month at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_months_before |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_months_before(GoodTimes.datetime) :: Enumerable.t
+  def stream_months_before(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_month_before datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going forward one year at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_years_after |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_years_after(GoodTimes.datetime) :: Enumerable.t
+  def stream_years_after(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_year_after datetime} end)
+  end
+
+
+  @doc """
+  Returns a Stream of UTC dates and times, starting with the given datetime,
+  going backward one year at a time.
+
+  ## Examples
+
+      iex> {{2015, 2, 27}, {18, 30, 45}} |> stream_years_before |> Enum.take 3
+      [{{2015, 2, 27}, {18, 30, 45}}, {{2015, 2, 27}, {18, 30, 46}},
+       {{2015, 2, 27}, {18, 30, 47}}]
+  """
+  @spec stream_years_before(GoodTimes.datetime) :: Enumerable.t
+  def stream_years_before(datetime) do
+    datetime |> Stream.unfold(fn datetime -> {datetime, a_year_before datetime} end)
+  end
+end
