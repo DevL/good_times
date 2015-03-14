58941cc4666056c16c59fb5a6933485c0edaaeba
Martin Svalin <martin@lite.nu>
2015-03-14 21:39:44 +0100

    Use doctest for GoodTimes.Convert, no explicit test cases

--- test/good_times/convert_test.exs
+++ test/good_times/convert_test.exs
@@ -1,20 +1,5 @@
 defmodule GoodTimes.ConvertTest do
   use ExUnit.Case
   import GoodTimes.Convert
-
-  @a_date {2015, 2, 27}
-  @a_time {18, 30, 45}
-  @a_datetime {@a_date, @a_time}
-
-  test "from_date" do
-    assert from_date(@a_date) == {@a_date, {0, 0, 0}}
-  end
-
-  test "to_date" do
-    assert to_date(@a_datetime) == @a_date
-  end
-
-  test "to_time" do
-    assert to_time(@a_datetime) == @a_time
-  end
+  doctest GoodTimes.Convert
 end
