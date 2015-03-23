81432f49811965540b6bc3f4c33f42aa689388b9
Lennart Fridén <lennart@devl.se>
2015-03-23 09:20:58 +0100

    Merge ~d and ~D sigils into ~D and the 'd' modifier.
    Replaces the ~d sigil with a 'd' modifer added to the ~D
    sigil.

--- lib/good_times/sigil.ex
+++ lib/good_times/sigil.ex
@@ -1,21 +1,10 @@
 defmodule GoodTimes.Sigil do
   @vsn GoodTimes.version
+  import GoodTimes.Convert, only: [to_date: 1]
 
   @moduledoc """
   Provides sigils for constructing dates, times, and datetimes.
   """
-
-  @doc """
-  Handles the sigil ~d. Returns a date tuple.
-
-  ## Examples
-
-      iex> ~d(2015-03-21)
-      {2015, 03, 21}
-  """
-  def sigil_d(date_string, []) do
-    date_string |> parse_into_tuple("-")
-  end
 
   @doc """
   Handles the sigil ~t. Returns a time tuple.
@@ -32,14 +21,47 @@ defmodule GoodTimes.Sigil do
   @doc """
   Handles the sigil ~D. Returns a datetime tuple.
 
+  Capable of parsing strings that comprise of
+  both a date and a time or just a date.
+
+  Use the modifier 'd' to return a date tuple.
+
   ## Examples
 
       iex> ~D(2015-03-21 11:51:24)
       {{2015, 03, 21}, {11, 51, 24}}
+
+      iex> ~D(2015-03-21)
+      {{2015, 03, 21}, {0, 0, 0}}
+
+      iex> ~D(2015-03-21 11:51:24)d
+      {{2015, 03, 21}}
+
+      iex> ~D(2015-03-21)d
+      {2015, 03, 21}
   """
-  def sigil_D(datetime_string, []) do
-    [date, time] = String.split(datetime_string)
-    {sigil_d(date, []), sigil_t(time, [])}
+  def sigil_D(string, []) do
+    string
+    |> String.split
+    |> convert
+  end
+
+  def sigil_D(string, [?d]) do
+    sigil_D(string, []) |> to_date
+  end
+
+  defp convert([date, time]) do
+    {
+      date |> parse_into_tuple("-"),
+      time |> parse_into_tuple(":")
+    }
+  end
+
+  defp convert([date]) do
+    {
+      date |> parse_into_tuple("-"),
+      {0, 0, 0}
+    }
   end
 
   defp parse_into_tuple(string, separator) do
--- test/good_times/sigil_test.exs
+++ test/good_times/sigil_test.exs
@@ -1,4 +1,31 @@
 defmodule GoodTimes.SigilTest do
   use ExUnit.Case, async: true
   doctest GoodTimes.Sigil, import: true
+  import GoodTimes.Sigil
+
+  @date {2015, 3, 22}
+  @datetime {{2015, 3, 22}, {9, 2, 45}}
+
+  test "~D given a date string returns a datetime" do
+    assert ~D(2015-03-22) == {{2015, 3, 22}, {0, 0, 0}}
+    assert ~D(2015-3-22) == {{2015, 3, 22}, {0, 0, 0}}
+  end
+
+  test "~D given a date and time string returns a datetime" do
+    assert ~D(2015-03-22 09:02:45) == @datetime
+    assert ~D(2015-3-22 09:02:45) == @datetime
+    assert ~D(2015-03-22 9:02:45) == @datetime
+    assert ~D(2015-3-22 9:02:45) == @datetime
+  end
+
+  test "~D given a date string and the 'd' modifier returns a date" do
+    assert ~D(2015-03-22)d == @date
+  end
+
+  test "~D given a date and time string and the 'd' modifier returns a date" do
+    assert ~D(2015-03-22 09:02:45)d == @date
+    assert ~D(2015-3-22 09:02:45)d == @date
+    assert ~D(2015-03-22 9:02:45)d == @date
+    assert ~D(2015-3-22 9:02:45)d == @date
+  end
 end
